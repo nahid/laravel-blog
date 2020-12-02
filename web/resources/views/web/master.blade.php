fe628e6be7a7b7dff27b2f3efd62e00cb7851268
Nahid Bin Azhar <nahid.dns@gmail.com>
2020-12-02 17:50:55 +0600

    added template skelton

---
+++ web/resources/views/web/master.blade.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="generator" content="Hugo 0.51">
+    <meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Ink - Crisp, minimal personal [blog theme for Hugo](https://github.com/knadh/hugo-ink) </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="alternate" type="application/rss+xml" href="/index.xml" title="Ink">
+    <meta itemprop="name" content="Ink">
+    <meta itemprop="description" content="">
+    <meta property="og:title" content="Ink">
+    <meta property="og:description" content="">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="/">
+    <meta property="og:updated_time" content="2019-04-19T21:37:58+05:30">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="Ink">
+    <meta name="twitter:description" content="">
+
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('css/css-normalize.css') }}">
+    <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('css/css-main.css') }}">
+    <link id="dark-scheme" rel="stylesheet" type="text/css" href="{{ asset('css/css-dark.css') }}">
+
+    <script src="{{ asset('js/dist-feather.min.js') }}"></script>
+    <script src="{{ asset('js/js-main.js') }}"></script>
+</head>
+<body>
+    <div class="container wrapper">
+        <div class="header">
+
+            <div class="avatar">
+                <a href="hugo-ink.netlify.html">
+                    <img src="images/free-vector-young-man-head-with-beard-avatar-character_24877-36786.jpg" alt="Ink"></a>
+            </div>
+
+            <h1 class="site-title"><a href="hugo-ink.netlify.html">NAH!D BIN AZHAR</a></h1>
+            <div class="site-description">
+                <p>Open Source Enthusiast, Code Artisan & Traveler</p>
+                <nav class="nav social">
+                    <ul class="flat">
+                        <li><a href="https://github.com/knadh/hugo-ink" title="Github"><i data-feather="github"></i></a></li>
+                        <li><a href="index.html" title="RSS"><i data-feather="rss"></i></a></li>
+                    </ul>
+                </nav>
+                <span class="scheme-toggle"><a href="#" id="scheme-toggle"></a></span>
+            </div>
+
+            <nav class="nav"><ul class="flat"><li>
+                        <a href="hugo-ink.netlify.html">Home</a>
+                    </li>
+
+                    <li>
+                        <a href="posts.html">All posts</a>
+                    </li>
+
+                    <li>
+                        <a href="about.html">About</a>
+                    </li>
+
+                    <li>
+                        <a href="tags.html">Tags</a>
+                    </li>
+
+                </ul></nav></div>
+
+
+        <div class="recent-posts section">
+            <div class="posts">
+
+
+
+                <div class="post">
+                    <div class="post-header">
+                        <div class="meta">
+                            <div class="date">
+                                <span class="day">11</span>
+                                <span class="rest">Mar 2019</span>
+                            </div>
+                        </div>
+                        <div class="matter">
+                            <h4 class="title small">
+                                <a href="post-8.html">Markdown Syntax Guide</a>
+                            </h4>
+                            <span class="description">
+
+                                            Sample article showcasing basic Markdown syntax and formatting for HTML elements.
+
+                                    </span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="post">
+                    <div class="post-header">
+                        <div class="meta">
+                            <div class="date">
+                                <span class="day">18</span>
+                                <span class="rest">Mar 2018</span>
+                            </div>
+                        </div>
+                        <div class="matter">
+                            <h4 class="title small">
+                                <a href="post-7.html">How to test dark mode?</a>
+                            </h4>
+                            <span class="description">
+
+                                            Here is how you can setup dark mode for Ink and test on various OS like iOS, Android, macOS and Windows 10.
+
+                                    </span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="post">
+                    <div class="post-header">
+                        <div class="meta">
+                            <div class="date">
+                                <span class="day">18</span>
+                                <span class="rest">Mar 2018</span>
+                            </div>
+                        </div>
+                        <div class="matter">
+                            <h4 class="title small">
+                                <a href="post-5.html">Typography</a>
+                            </h4>
+                            <span class="description">
+
+
+    Lid est laborum et dolorum fuga. Et harum quidem rerum facilis est et expeditasi distinctio. Nam libero tempore, cum so...
+
+                                    </span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="post">
+                    <div class="post-header">
+                        <div class="meta">
+                            <div class="date">
+                                <span class="day">18</span>
+                                <span class="rest">Mar 2018</span>
+                            </div>
+                        </div>
+                        <div class="matter">
+                            <h4 class="title small">
+                                <a href="post-6.html">Hugo shortcodes</a>
+                            </h4>
+                            <span class="description">
+
+                                            Here is a demo of all shortcodes available in Hugo.
+
+                                    </span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="post">
+                    <div class="post-header">
+                        <div class="meta">
+                            <div class="date">
+                                <span class="day">18</span>
+                                <span class="rest">Mar 2018</span>
+                            </div>
+                        </div>
+                        <div class="matter">
+                            <h4 class="title small">
+                                <a href="post-4.html">Getting Started with Traveling Ultralight</a>
+                            </h4>
+                            <span class="description">
+
+                                            Start by getting a small backpack (less than 20 liters) and then just travel with what fits in that.
+
+                                    </span>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <ul class="pagination"><li class="page-item page-prev">
+
+                    </li>
+                    <li class="page-item page-next">
+
+                        <a href="2.html" class="page-link" aria-label="Next"><span aria-hidden="true">Next page &rarr;</span></a>
+
+                    </li>
+                </ul></div>
+        </div>
+    </div>
+    <div class="footer wrapper">
+        <nav class="nav"><div>2020  &copy; Copyright notice |  <a href="https://github.com/knadh/hugo-ink">Ink</a> theme on <a href="https://gohugo.io">Hugo</a></div>
+        </nav></div>
+
+
+    <script type="application/javascript">
+    var doNotTrack = false;
+    if (!doNotTrack) {
+        window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+        ga('create', 'UA-123-45', 'auto');
+
+        ga('send', 'pageview');
+    }
+    </script><script async src="js/analytics.js"></script><script>feather.replace()</script>
+</body>
+</html>
